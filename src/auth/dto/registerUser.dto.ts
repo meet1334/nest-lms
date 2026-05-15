997f151dddb8a1694e2a9e3228fd135ecbd09a3d
meetvaghasiya2023 <meet.vaghasiya@esparkbizmail.com>
2026-05-15 17:41:46 +0530

    Add validation using package

--- src/auth/dto/registerUser.dto.ts
+++ src/auth/dto/registerUser.dto.ts
@@ -1,6 +1,20 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator'
+
 export class RegisterDto {
+
+    @IsString()
+    @IsNotEmpty()
     fname: string;
+
+    @IsString()
+    @IsNotEmpty()
     lname: string;
+
+    @IsEmail()
+    @IsNotEmpty()
     email: string;
+
+    @IsString()
+    @IsNotEmpty()
     password: string;
 }
